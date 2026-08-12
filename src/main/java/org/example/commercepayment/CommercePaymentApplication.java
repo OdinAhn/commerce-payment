2247858e05eb723e67c0725543ba5971c6cd2dab
choiyongbum <72883647+choiyongbum@users.noreply.github.com>
2026-08-12 16:24:32 +0900

    chore: 프로젝트 초기 구조 설정

---
+++ src/main/java/org/example/commercepayment/CommercePaymentApplication.java
@@ -0,0 +1,13 @@
+package org.example.commercepayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommercePaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommercePaymentApplication.class, args);
+    }
+
+}
